1f1bc9f72fdbe746d2a43194b5a50b202b5eb115
AGMA96 <oregan@agma96>
2022-03-14 15:58:31 +0900

    add_items_to_playlist.py ・ファイルパスを現状読み込み可能なパスに修正 ・outputの出力ディレクトリを変更 ・変数名をnew_releases用、related_top_tracks用のどちらにも合うように修正

--- control/playlists/add_items_to_playlist.py
+++ control/playlists/add_items_to_playlist.py
@@ -11,22 +11,29 @@ def split_list(list):
     for idx in range(0, len(list), length):
         yield list[idx:idx + length]
 
-top_tracks = {}
+tracks = {}
 
 ## variables #
 tracks_ids_file_name = 'new_releases_tracks_ids'
-playlist_id = '00XAPpcIZVmq5Urym5AQIR'
-############# 
+#tracks_ids_file_name = 'artists_top_tracks_ids'
 
-TRACKS_PATH = os.path.normpath(os.path.join(os.getcwd(),'..', '..', 'Tracks', 'input', tracks_ids_file_name +'.json'))
+if tracks_ids_file_name == 'new_releases_tracks_ids':
+    playlist_id = '00XAPpcIZVmq5Urym5AQIR'
+    output_directory = 'NewReleases'
+elif tracks_ids_file_name == 'artists_top_tracks_ids':
+    playlist_id = '5XarPyt9vxNWIbjKzGmQ6f'
+    output_directory = 'RelatedArtistsTopTracks'
+#############
+
+TRACKS_PATH = os.path.normpath(os.path.join(os.getcwd(), 'jsons', 'Tracks', 'input', tracks_ids_file_name +'.json'))
 with open(TRACKS_PATH, 'r', encoding='utf-8') as tracks_file:
-    top_tracks = json.load(tracks_file)
-print('artists top tracks: ', str(len(top_tracks)))
+    tracks = json.load(tracks_file)
+print(tracks_ids_file_name, ': ', str(len(tracks)))
 
-ADDED_TRACKS_PATHS = glob.glob(os.path.join(os.getcwd(), '..', 'output', '*.json'))
+ADDED_TRACKS_PATHS = glob.glob(os.path.join(os.getcwd(), 'jsons', 'Playlists', 'output', output_directory, '*.json'))
 # TODO JSON構造化 1000曲/1ファイル
 '''
-[file_name] = 'playlist_id'.json
+[file_name] = '(playlist_id)_(5 digits)'.json
 {
     "items":[
         {
@@ -53,17 +60,17 @@ ADDED_TRACKS_PATHS = glob.glob(os.path.join(os.getcwd(), '..', 'output', '*.json
 
 # got added tracks from before excuted
 added_tracks_before = set()
-for related_artists_path in ADDED_TRACKS_PATHS:
-    with open(related_artists_path, 'r', encoding='utf-8') as added_tracks_file:
-        added_tracks_json = json.load(added_tracks_file)
-    for items in added_tracks_json['items']:
+for added_tracks_path in ADDED_TRACKS_PATHS:
+    with open(added_tracks_path, 'r', encoding='utf-8') as added_tracks_file:
+        added_tracks_before_json = json.load(added_tracks_file)
+    for items in added_tracks_before_json['items']:
         tracks_ids_file_name = {track['id'] for track in items['tracks']}
         added_tracks_before.update(tracks_ids_file_name)
 
 print('already added tracks : ', str(len(added_tracks_before)))
 
 # 追加済みのトラックIDを差し引く
-target_tracks = {id: name for id, name in top_tracks.items() if id not in added_tracks_before}
+target_tracks = {id: name for id, name in tracks.items() if id not in added_tracks_before}
 
 scope="playlist-modify-private"
 sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope))
@@ -83,15 +90,19 @@ for track_list in splited_track_list[:10]: # add only 1,000 tracks
 
     print('added tracks(snapshot_id): ', response['snapshot_id'])
 
-    with open(os.path.join(os.getcwd(), response['snapshot_id'] + '.json'), 'w', encoding='utf-8') as response_file:
+    snapshot_json_path = os.path.join(os.getcwd(), 'jsons', 'Playlists', 'AddItemstoPlaylist', response['snapshot_id'] + '.json')
+    with open(snapshot_json_path, 'w', encoding='utf-8') as response_file:
         json.dump(response, response_file, indent=4, ensure_ascii=False)
         print('dumped! response : ',response_file)
  
     time.sleep(1)
 
-with open(os.path.join(os.getcwd(), '..', 'output', playlist_id + '.json'), 'w', encoding='utf-8') as added_tracks_file:
+
+# create added_tracks_file
+new_added_tracks_path= os.path.join(os.getcwd(), 'jsons', 'Playlists', 'output', output_directory, playlist_id + '_{:05d}.json'.format(len(ADDED_TRACKS_PATHS) + 1))
+with open(new_added_tracks_path, 'w', encoding='utf-8') as added_tracks_file:
     json.dump(added_tracks, added_tracks_file, indent=4, ensure_ascii=False)
-    print('dumped! ',added_tracks_file)
+    print('update! ',added_tracks_file)
    
 
     
